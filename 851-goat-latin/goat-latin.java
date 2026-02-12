4d376aa175068f156da2b05b8d6824bfb0666725
Venkatachalam S <venkatachalamsubramanian23@gmail.com>
2026-02-12 21:03:40 +0530

    Time: 7 ms (17.19%) | Memory: 43.5 MB (34.75%) - LeetSync

---
+++ 851-goat-latin/goat-latin.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String toGoatLatin(String sentence) {
+        String[] str = sentence.trim().split(" ");
+        String a = "a";
+        System.out.println(Arrays.toString(str));
+        for(int itr = 0; itr < str.length; itr++){
+            if(isVowel(str[itr].charAt(0))){
+                str[itr] += "ma";
+            }
+            else{
+                str[itr] = str[itr].substring(1) + str[itr].charAt(0) + "ma";
+            }
+            str[itr] += a;
+            a += "a";
+        }
+
+        return String.join(" ", str);
+    }
+    static boolean isVowel(char ch){
+        if(ch == 'a' || ch == 'e' || ch == 'o' || ch == 'i' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+            return true;
+        }
+        return false;
+    }
+}
