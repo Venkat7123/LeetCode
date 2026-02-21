4b958a031da706934177523f7c9e8a76477cc248
Venkatachalam S <venkatachalamsubramanian23@gmail.com>
2026-02-21 10:20:12 +0530

    Time: 3 ms (94.51%) | Memory: 46.6 MB (78.81%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,41 +1,40 @@
 class Solution {
     public String convert(String s, int numRows) {
-        int num1 = (2*numRows)-2;
-        int num2 = 0;
-        int itr;
+        int gap1 = (numRows - 1) * 2;
+        int gap2 = 0;
         int len = s.length();
-        if(len <= numRows || numRows == 1) return s;
-        
-        char[] arr = s.toCharArray();
-        StringBuilder res = new StringBuilder();
-       
-        for(itr = 0 ; itr < numRows; itr++){
-            
+        if(len < numRows || numRows == 1){
+            return s;
+        }
 
-            int end = itr;          
-            res.append(arr[end]);          
-            while(end < len){
-                if(itr == 0){
-                    end+= num1;
-                    if(end > len-1) break;
-                    res.append(arr[end]);
+        char[] str = s.toCharArray();
+        StringBuilder res = new StringBuilder();
+
+        for(int i = 0; i < numRows; i++){
+            int ind = i;
+            res.append(s.charAt(ind));
+            while(ind < len){
+                if(i == 0){
+                    ind += gap1;
+                    if(ind > len - 1) break;
+                    res.append(s.charAt(ind));
                 }
-                else if(itr == numRows -1 ){
-                    end+= num2;
-                    if(end > len-1) break;
-                    res.append(arr[end]);
+                else if(i == numRows - 1){
+                    ind += gap2;
+                    if(ind > len - 1) break;
+                    res.append(s.charAt(ind));
                 }
                 else{
-                    end+= num1;
-                    if(end > len-1) break;
-                    res.append(arr[end]);
-                    end+= num2;
-                    if(end > len-1) break;
-                    res.append(arr[end]);
+                    ind += gap1;
+                    if(ind > len - 1) break;
+                    res.append(s.charAt(ind));
+                    ind += gap2;
+                    if(ind > len - 1) break;
+                    res.append(s.charAt(ind));
                 }
             }
-            num1 -= 2;
-            num2 += 2;
+            gap1-=2;
+            gap2+=2;
         }
         return res.toString();
 
