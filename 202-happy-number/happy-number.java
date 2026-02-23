f4cd96bbea47aee9b2b2546f656b03305233351d
Venkatachalam S <venkatachalamsubramanian23@gmail.com>
2026-02-23 18:55:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (65.54%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isHappy(int n) {
+        if(n == 1 || n == 7) return true;
+        else if(n < 10) return false;
+        int sum = 0;
+        while(n > 0){
+            int dig = n % 10;
+            sum += dig * dig;
+            n /= 10;
+        }
+        return isHappy(sum);
+    }
+
+}
