46363e2c5e410c6860a09d6c1eebbb0b88e7ca83
Venkatachalam S <venkatachalamsubramanian23@gmail.com>
2026-04-23 21:22:38 +0530

    Time: 19 ms (95.07%) | Memory: 124.2 MB (22.42%) - LeetSync

--- 2721-sum-of-distances/sum-of-distances.java
+++ 2721-sum-of-distances/sum-of-distances.java
@@ -1,34 +1,32 @@
 class Solution {
     public long[] distance(int[] nums) {
-        int n = nums.length;
-        long[] ans = new long[n];
-
-        Map<Integer, List<Integer>> mp = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            mp.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        HashMap<Integer,ArrayList<Integer>> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+           if(!map.containsKey(nums[i])) {
+                map.put(nums[i], new ArrayList<>());
+            }
+            map.get(nums[i]).add(i);
         }
 
-        for (List<Integer> pos : mp.values()) {
-
+       long[] ans = new long[nums.length];
+        for(ArrayList<Integer> indices : map.values()) {
+            int n = indices.size();
             long sum = 0;
-            for (int x : pos) sum += x;
-
-            long leftSum = 0;
-            int m = pos.size();
-
-            for (int i = 0; i < m; i++) {
-                long rightSum = sum - leftSum - pos.get(i);
-
-                long left  = (long) pos.get(i) * i - leftSum;
-                long right = rightSum - (long) pos.get(i) * (m - i - 1);
-
-                ans[pos.get(i)] = left + right;
-
-                leftSum += pos.get(i);
+            for(int i = 0; i < n; i++) {
+                sum += indices.get(i);
+            }
+            long leftsum = 0;
+            long rightsum = sum;
+            long currsum = 0;
+            for(int i = 0; i < n; i++) {
+                currsum = 0;
+                currsum += (long)i*(long)indices.get(i) - leftsum;
+                currsum += rightsum - (long)(n-i)*(long)indices.get(i);
+                leftsum += (long)indices.get(i);
+                rightsum -= (long)indices.get(i);
+                ans[indices.get(i)] = currsum;
             }
         }
-
         return ans;
     }
 }
