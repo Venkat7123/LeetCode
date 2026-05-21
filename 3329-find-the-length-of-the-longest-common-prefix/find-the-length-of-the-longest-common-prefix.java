a0fed3d90b9a8fbba98d54a03679ea4b313dbe05
Venkatachalam S <venkatachalamsubramanian23@gmail.com>
2026-05-21 13:54:42 +0530

    Time: 38 ms (96.47%) | Memory: 84.3 MB (37.52%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
@@ -1,44 +1,60 @@
 class Solution {
-    public int digits(int x) {
-        int cnt = 0;
-        while(x > 0) {
-            cnt++;
-            x /= 10;
+
+    class Node {
+        Node[] child;
+        boolean isEnd;
+
+        Node() {
+            child = new Node[10];
+            isEnd = false;
         }
-        return cnt;
+    }
+
+    void insert(String word, Node root) {
+        Node temp = root;
+
+        for (char ch : word.toCharArray()) {
+            int idx = ch - '0';
+
+            if (temp.child[idx] == null) {
+                temp.child[idx] = new Node();
+            }
+
+            temp = temp.child[idx];
+        }
+
+        temp.isEnd = true;
+    }
+
+    int check(String str, Node root) {
+        Node temp = root;
+        int idx = 0;
+
+        while (idx < str.length()) {
+            int i = str.charAt(idx) - '0';
+
+            if (temp.child[i] != null) {
+                temp = temp.child[i];
+                idx++;
+            } else {
+                break;
+            }
+        }
+
+        return idx;
     }
 
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        HashSet<Integer> prefixes =new HashSet<>();
+        Node root = new Node();
 
-        // storing all prefixes of arr1
-        for(int num : arr1) {
-            int x = num;
-            while(x > 0) {
-                prefixes.add(x);
-                x /= 10;
-            }
+        for (int x : arr2) {
+            insert(String.valueOf(x), root);
         }
 
         int ans = 0;
 
-        // check prefixes of arr2 numbers
-        for(int num : arr2) {
-            int x = num;
-            int len = digits(num);
-
-            // checking from larger => smaller
-            while(x > 0) {
-                if(prefixes.contains(x)) {
-                    ans = Math.max(ans, len);
-                    // first match is the longest
-                    // so we stop
-                    break;
-                }
-
-                x /= 10;
-                len--;
-            }
+        for (int x : arr1) {
+            ans = Math.max(ans, check(String.valueOf(x), root));
         }
 
         return ans;
