f20a2ad9055ccebedce849e223d25474916c2bd2
Venkatachalam S <venkatachalamsubramanian23@gmail.com>
2026-06-05 09:43:46 +0530

    Time: 42 ms (100.00%) | Memory: 46.8 MB (70.37%) - LeetSync

--- 4128-total-waviness-of-numbers-in-range-ii/total-waviness-of-numbers-in-range-ii.java
+++ 4128-total-waviness-of-numbers-in-range-ii/total-waviness-of-numbers-in-range-ii.java
@@ -1,98 +1,84 @@
 class Solution {
+    // directions
+    static final int NONE = 0, UP = 1, DOWN = 2, FLAT = 3;
 
-    static class Node {
-        long waviness;
-        long cnt;
-
-        Node(long waviness, long cnt) {
-            this.waviness = waviness;
-            this.cnt = cnt;
-        }
-    }
-
-    private String s;
-    private Node[][][][][] dp;
-    private boolean[][][][][] vis;
-
-    private Node dfs(int pos, int tight, int started, int prev2, int prev1) {
-
-        if (pos == s.length()) {
-            return new Node(0, 1);
-        }
-
-        if (vis[pos][tight][started][prev2][prev1]) {
-            return dp[pos][tight][started][prev2][prev1];
-        }
-
-        vis[pos][tight][started][prev2][prev1] = true;
-
-        long totalWaviness = 0;
-        long totalCnt = 0;
-
-        int limit = (tight == 1) ? s.charAt(pos) - '0' : 9;
-
-        for (int d = 0; d <= limit; d++) {
-
-            int nextTight = (tight == 1 && d == limit) ? 1 : 0;
-
-            if (started == 0 && d == 0) {
-
-                Node child = dfs(pos + 1, nextTight, 0, 10, 10);
-
-                totalWaviness += child.waviness;
-                totalCnt += child.cnt;
-            } else {
-
-                int nPrev2;
-                int nPrev1;
-                long add = 0;
-
-                if (started == 0) {
-
-                    nPrev2 = 10;
-                    nPrev1 = d;
-
-                } else {
-
-                    if (prev2 != 10) {
-
-                        boolean peak =(prev1 > prev2 && prev1 > d);
-                        boolean valley =(prev1 < prev2 && prev1 < d);
-
-                        if (peak || valley) {
-                            add = 1;
-                        }
-                    }
-
-                    nPrev2 = prev1;
-                    nPrev1 = d;
-                }
-
-                Node child = dfs(pos + 1,nextTight,1,nPrev2,nPrev1);
-
-                totalWaviness += child.waviness + add * child.cnt;
-                totalCnt += child.cnt;
-            }
-        }
-
-        return dp[pos][tight][started][prev2][prev1] =
-                new Node(totalWaviness, totalCnt);
-    }
-
-    private long solve(long n) {
-
-        if (n <= 0) {
-            return 0;
-        }
-
-        s = Long.toString(n);
-        dp = new Node[17][2][2][11][11];
-        vis = new boolean[17][2][2][11][11];
-
-        return dfs(0, 1, 0, 10, 10).waviness;
-    }
+    int[] digits;
+    Map<Long, long[]> memo;
 
     public long totalWaviness(long num1, long num2) {
         return solve(num2) - solve(num1 - 1);
+    }
+
+    long solve(long num) {
+        if (num <= 0)
+            return 0;
+        memo = new HashMap<>();
+
+        // split num into digits
+        String s = Long.toString(num);
+        digits = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            digits[i] = s.charAt(i) - '0';
+        }
+
+        return dp(0, 0, NONE, true, false)[1];
+    }
+
+    // returns [count of numbers, total waviness sum]
+    long[] dp(int pos, int prev, int dir, boolean tight, boolean started) {
+        if (pos == digits.length) {
+            return new long[] { started ? 1 : 0, 0 };
+        }
+
+        long key = ((long) pos * 10 + prev) * 4 + dir;
+        key = key * 2 + (tight ? 1 : 0);
+        key = key * 2 + (started ? 1 : 0);
+        if (memo.containsKey(key))
+            return memo.get(key);
+
+        int limit = tight ? digits[pos] : 9;
+        long count = 0, waveSum = 0;
+
+        for (int d = 0; d <= limit; d++) {
+            boolean newTight = tight && (d == limit);
+            boolean newStarted = started || (d != 0);
+
+            // determine new prev and direction
+            int newPrev, newDir;
+            if (!newStarted) {
+                // still leading zeros
+                newPrev = 0;
+                newDir = NONE;
+            } else if (!started) {
+                // first real digit
+                newPrev = d;
+                newDir = NONE;
+            } else {
+                newPrev = d;
+                if (d > prev)
+                    newDir = UP;
+                else if (d < prev)
+                    newDir = DOWN;
+                else
+                    newDir = FLAT;
+            }
+
+            // check if prev digit becomes a peak or valley
+            long extra = 0;
+            if (started) {
+                if (dir == UP && d < prev)
+                    extra = 1; // prev was peak
+                if (dir == DOWN && d > prev)
+                    extra = 1; // prev was valley
+            }
+
+            long[] sub = dp(pos + 1, newPrev, newDir, newTight, newStarted);
+            count += sub[0];
+            waveSum += sub[1] + extra * sub[0];
+        }
+
+        long[] res = new long[] { count, waveSum };
+        memo.put(key, res);
+        return res;
     }
 }
