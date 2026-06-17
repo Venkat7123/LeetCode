cbde3fa5b579b14c9b9dfa5e731ecdc12c12225e
Venkatachalam S <venkatachalamsubramanian23@gmail.com>
2026-06-17 12:42:15 +0530

    Time: 29 ms (73.08%) | Memory: 48.1 MB (26.92%) - LeetSync

--- 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.java
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.java
@@ -1,38 +1,35 @@
 class Solution {
     public char processStr(String s, long k) {
-        int n = s.length();
-        long[] lens = new long[n];
-        long len = 0;
-
-        for (int i = 0; i < n; i++) {
+        long m = 0;
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c == '*')
-                len = Math.max(len - 1, 0L);
-            else if (c == '#')
-                len *= 2;
-            else if (c != '%')
-                len++;
-            
-            lens[i] = len;
+            if (c == '*') {
+                m = Math.max(0, m - 1);
+            } else if (c == '#') {
+                m <<= 1;
+            } else if (c != '%') {
+                m += 1;
+            }
         }
-
-        if (k >= len) return '.';
-
-        for (int i = n - 1; ; i--) {
+        if (k >= m) {
+            return '.';
+        }
+        for (int i = s.length() - 1;; i--) {
             char c = s.charAt(i);
-            switch (c) {
-                case '*':
-                    break;
-                case '#':
-                    if (k >= lens[i] / 2)
-                        k -= lens[i] / 2;
-                    break;
-                case '%':
-                    k = lens[i] - 1 - k;
-                    break;
-                default:
-                    if (lens[i] == k + 1)
-                        return c;
+            if (c == '*') {
+                m += 1;
+            } else if (c == '#') {
+                m /= 2;
+                if (k >= m) {
+                    k -= m;
+                }
+            } else if (c == '%') {
+                k = m - 1 - k;
+            } else {
+                m -= 1;
+                if (k == m) {
+                    return c;
+                }
             }
         }
     }
